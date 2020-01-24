3a6a8ce90976f64760b4b22ac604f3e4c69ce845
Jack Dunning <JackStunning9001@gmail.com>
2020-01-24 10:52:48 -0800

    Add more css styling

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
   $("#button").click(function(event){
     event.preventDefault();
     var number = $("input#number").val();
+    var name = $("input#name").val();
+
     $("#result").append()
     
     if(number.includes(3)){
-      $("#result").append(`I'm sorry Dave, I'm afraid I can't do that. <br>`)
+      $("#result").append(`I'm sorry ${name}, I'm afraid I can't do that. <br>`)
       
     } else if(number.includes(2)){
       $("#result").append(`Boop <br>`)
@@ -18,7 +20,7 @@ $(document).ready(function(){
     } else {
       $("#result").append(`Beep <br>`)
       $("#result").append(`Boop <br>`)
-      $("#result").append(`I'm sorry Dave, I'm afraid I can't do that. <br>`)
+      $("#result").append(`I'm sorry ${name}, I'm afraid I can't do that. <br>`)
       for(var i = 4; i <= number; i++) {
         $("#result").append(`${i} <br>`)
       }
