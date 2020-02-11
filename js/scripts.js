c3485c1472b7d2e26924037c09a05753a1e327c9
Jack Dunning <JackStunning9001@gmail.com>
2020-02-10 19:09:45 -0800

    Finish Project

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+function beepBoop(num, name) {
+  if(num.includes(3)){
+    return `Beep <br>Boop <br>I'm sorry, ${name}. I'm afraid I can't do that. <br>`
+  } else if(num.includes(2)) {
+    return `Beep <br>Boop <br>`
+  } else if(num.includes(1)) {
+    return `Beep <br>`
+  } else {
+    let arr = [];
+    arr = [`Beep<br>`, `Boop<br>`, `I'm sorry, ${name}. I'm afraid I can't do that.<br>`];
+    for(var i = 4; i <= num; i++) {
+      arr.push(`${i} <br>`);
+    }
+    return arr
+  }
+}
+
+
 $(document).ready(function(){
 
   //button clicked function
@@ -5,47 +23,16 @@ $(document).ready(function(){
     event.preventDefault();
     var number = $("input#number").val();
     var name = $("input#name").val();
+    $("#regular").empty() 
+    $("#regular").append(beepBoop(number, name));
 
-    var arr = [];
+  $("#reverseButton").click(function(event){
+    event.preventDefault();
+    var number = $("input#number").val();
+    var name = $("input#name").val();
+    $("#regular").empty();
+    $("#regular").append(beepBoop(number, name).reverse());
     
-    if(number.includes(3)){
-      arr.push(`I'm sorry, ${name}. I'm afraid I can't do that. <br>`)
-      $("#result").append(arr[0])
-      
-    } else if(number.includes(2)){
-      arr.push(`Boop <br>`)
-      $("#result").append(arr[0])
-      
-    } else if (number.includes(1)){
-      arr.push(`Beep <br>`)
-      $("#result").append(arr[0])
-      
-    } else {
-      arr.push(`Beep <br>`)
-      arr.push(`Boop <br>`)
-      arr.push(`I'm sorry, ${name}. I'm afraid I can't do that. <br>`)
-      $("#regular").append(arr[0])
-      $("#regular").append(arr[1])
-      $("#regular").append(arr[2])
-      for(var i = 4; i <= number; i++) {
-        arr.push(`${i}`)
-        $("#regular").append(`${arr[i - 1]} <br>`)
-      }
-    }
-
-    $("#reverseButton").click(function(event){
-      event.preventDefault();
-      $("#regular").toggle();
-      var revArr = arr.reverse();
-      console.log(arr)
-      console.log(revArr)
-
-      for (var i = 0; i < revArr.length - 3; i++) {
-        $("#reverse").append(`${revArr[i]} <br>`)
-      }
-      $("#reverse").append(revArr[arr.length - 3])
-      $("#reverse").append(revArr[arr.length - 2])
-      $("#reverse").append(revArr[arr.length - 1])
-    })
+  })
   })
 })
